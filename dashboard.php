2f7df0821a4e28a737800900a3334357f2a75992
Jarrett Croll <jarrett.croll@gmail.com>
2012-08-18 19:40:57 -0700

    Changed include syntax to work with remote server

--- dashboard.php
+++ dashboard.php
@@ -37,7 +37,7 @@ $response = \Httpful\Request::get($url)->send();*/
 <body>
 <!--[if lt IE 7]><p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
 
-<?php include("navbar.php") ?>
+<?php include "navbar.php" ?>
 
     <div class="container-fluid">
         <h1>Dashboard</h1>
