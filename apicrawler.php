e2036d64b69e9cf558588d18cead34d593386482
Jarrett Croll <jarrett.croll@gmail.com>
2012-08-19 12:00:42 -0700

    Updated

--- apicrawler.php
+++ apicrawler.php
@@ -1,7 +1,8 @@
 <?php
-/*include('vendor/httpful.phar');
+phpinfo(); die;
+include('vendor/httpful.phar');
 
-extract($_GET);
+/*extract($_GET);
 
 $url = 'http://www.ohloh.net/accounts/'.$account_id.'.xml?api_key=xbtJiPKjbBMgpz79ftYIcA';
 
@@ -17,45 +18,12 @@ $response = \Httpful\Request::get($url)->send();*/
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
 	<title></title>
-	<meta name="description" content="">
-	<meta name="author" content="">
-
-	<meta name="viewport" content="width=device-width">
-
-	<link rel="stylesheet" href="css/bootstrap.min.css">
-	<style>
-	body {
-	  padding-top: 60px;
-	  padding-bottom: 40px;
-	}
-	</style>
-	<link rel="stylesheet" href="css/bootstrap-responsive.min.css">
-	<link rel="stylesheet" href="css/style.css">
-
-	<script src="js/libs/modernizr-2.5.3-respond-1.1.0.min.js"></script>
+    <?php include "header.php" ?>
 </head>
 <body>
 <!--[if lt IE 7]><p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
 
-    <div class="navbar navbar-fixed-top">
-      <div class="navbar-inner">
-        <div class="container">
-          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </a>
-          <a class="brand" href="/">Codefolio API Crawler</a>
-          <div class="nav-collapse">
-            <ul class="nav">
-              <li class="active"><a href="/">Home</a></li>
-              <li><a href="#about">About</a></li>
-              <li><a href="#contact">Contact</a></li>
-            </ul>
-          </div><!--/.nav-collapse -->
-        </div>
-      </div>
-    </div>
+    <?php include "navbar.php" ?>
 
     <div class="container">
 
